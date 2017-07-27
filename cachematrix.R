36a94ae6e8a2a839f0af059127b13ea5a74b79e0
Richa Agrawal <richa.agrawal@gmail.com>
2017-07-27 17:52:02 +0530

    adding cache matrix files to HUB

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Using Lexical Scoping of [R] to provide caching
 
-## Write a short comment describing this function
+## makeCacheMatrix function allows to cache a matrix which is passed as a formal argument to this function
+## and it's inverse matrix as well. This funtion provides the get and set functions to 
+## access the matrix and inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix<- function(y=matrix()) 
+{
+        inverse<- NULL
+    	  set <- function(x) {
+                y <<- x
+                inverse<<- NULL
+        }
+        get <- function() { y }
+        setinv <- function() { inverse<<-solve(y) }
+        getinv <- function() { inverse}
+        list(setmatrix = set, getmatrix = get,
+             setinverse = setinv  ,
+             getinverse = getinv )
+ }
 
-}
 
+## cacheSolve allows to return the cached or normal inverse matrix 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(y,...)
+{
+	inv<-NULL
+	inv<-y$getinverse()
+	if(!is.null(inv))
+	{ 	
+		message("Getting cached Inverse")
+      	return(inv)
+      }
+	# set the inverse and then fetch it
+      y$setinverse()
+      inv<-y$getinverse()
+	message("Getting Inverse")
+	inv
 }
